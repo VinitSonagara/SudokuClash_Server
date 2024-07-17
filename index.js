8e5a6fc4fe63c50d298c211dc6695acdbd738255
Vinit Sonagara <vinitsonagara@gmail.com>
2024-07-17 18:52:50 +0530

    Add cors realted code for express routes & socket io

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 import express from 'express';
+import { Server } from 'socket.io';
+import cors from 'cors';
 import generateUniqueCode from './utils/generateUniqueCode.js';
 
 const app = express();
+app.use(cors());
+
+const server = app.listen(3000, () => {
+	console.log('server running at http://localhost:3000');
+});
+
+const io = new Server(server, {
+	cors: {
+		origin: '*', // Allow all origins, you can specify a particular domain if needed
+		methods: ['GET', 'POST'],
+		allowedHeaders: ['Content-Type'],
+		credentials: true,
+	},
+});
 
 app.get('/create', (req, res) => {
 	const code = generateUniqueCode(6);
 	res.json({ code });
 });
 
-app.listen(3000, () => {
-	console.log('server running at http://localhost:3000');
+io.on('connection', (socket) => {
+	console.log('a user connected:', socket.id);
 });
